a3a1e3b2755b513740f5d53630730b261c6b6115
Zaha Rehman <zaharehman788@gmail.com>
2024-10-04 10:25:25 +0500

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@ defineProps({
     <h1 class="green">{{ msg }}</h1>
     <h3>
       You’ve successfully created a project with
-      and talha give a beaautiful fisr commit to this project
+      and zaha give a beaautiful fisr commit to this project
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
