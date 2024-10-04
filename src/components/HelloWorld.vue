a06b9d95b53db3eb41939d6d3e6059106b028c00
SalmanMani86 <salman.hanif524@gmail.com>
2024-10-04 10:34:35 +0500

    Name Add

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <h1 class="green">{{ msg }} Salman Ali</h1>
     <h3>
       You’ve successfully created a project with
       and hurara give a beaautiful fisr commit to this project
